e6b4fc62009d2c8ae0b28ff7fe30f096dfc24771
fatihfilizol <fatihfilizol@gmail.com>
2022-09-17 15:18:04 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Fatih Filizol on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
